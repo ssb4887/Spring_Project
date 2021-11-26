a475c5822214f3ca03c92dc75a0e706957d570c3
seok <ssb4887@naver.com>
2021-11-26 16:49:21 +0900

    2021-11-26 16:50 commit

--- src/main/java/com/bbs/util/Mail.java
+++ src/main/java/com/bbs/util/Mail.java
@@ -14,6 +14,7 @@ public class Mail extends Authenticator {
 		pw = "Qazw1234!!";
 	}
 		
+	
 	protected PasswordAuthentication getPasswordAuthentication() {
 	
 		return new PasswordAuthentication(id, pw);
